cfe71cb05cedfd2e5ad98ef6698f5039a37456c2
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-01-18 14:26:15 -0800

    Add files via upload

---
+++ ASSIGNMENT02/src/assignment2/Fibonacci.java
@@ -0,0 +1,26 @@
+package assignment2;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int first_n;
+		int second_n;
+		first_n = 1;
+		second_n = 1;
+		System.out.print(first_n + " " + second_n + " ");
+	
+	
+	    for (int i = 0; i < 10; i++) {
+	    	int next_n = first_n + second_n;
+	    	first_n = second_n;
+	    	second_n = next_n;
+	    	System.out.print(second_n + " " );
+	    }
+	
+	
+	
+	
+	}
+
+}
