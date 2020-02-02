222f6b545e715df15b936d8b5733bee176e1e13e
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-02-02 15:30:56 -0800

    Add files via upload

---
+++ quruxbadan.java
@@ -0,0 +1,19 @@
+
+public class quruxbadan {
+
+	public static void main(String[] args) {
+		System.out.println(repl("ram", 4));
+
+	}
+
+	private static String repl(String string, int i, int n, String s) {
+		String value = "";
+		for (int i = 1; i <= n; i++) {
+			value = value + s;
+		}
+	
+        return value;
+ 
+	}
+
+}
