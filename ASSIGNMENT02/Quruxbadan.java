da8624dcdc9a71dbf905cb44ebef3138d8bdf17c
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-05-12 19:28:52 -0700

    Add files via upload

---
+++ ASSIGNMENT02/Quruxbadan.java
@@ -0,0 +1,12 @@
+
+public class Quruxbadan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNum = 15;
+		myNum = 20;  // My is now 20
+		System.out.println(myNum);		
+
+	}
+
+}
