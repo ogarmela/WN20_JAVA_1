503554150abe110d9f7d769635baae00c1741872
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-06-17 14:59:51 -0700

    Add files via upload

---
+++ ASSIGNMENT02/octagon.java
@@ -0,0 +1,28 @@
+package muno;
+
+public class octagon {
+	/** Method method */
+	public static void main(String[] args) throws CloneNotSupportedException {
+		// Create an Octagon object
+		Octagon octagon1 = new Octagon(5);
+
+		// Display area and perimeter of object
+		System.out.println("\nOctagon:\nArea: " + octagon1.getArea() + 
+			"\nPerimeter: " + octagon1.getPerimeter());
+
+		// Create new object using the clone method
+		System.out.println("Cloning Octagon...");
+		Octagon octagon2 = (Octagon)octagon1.clone();
+
+		// Compare the two object using compareTo method
+		int result = (octagon1.compareTo(octagon2));
+		if (result == 1)
+			System.out.println("Octagon is greather than its clone.");
+		else if (result == -1)
+			System.out.println("Octagon is less than its clone."); 
+		else
+			System.out.println("Octagon is equal to its clone.");
+	}
+}
+
+
